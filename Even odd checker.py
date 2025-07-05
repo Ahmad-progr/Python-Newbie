b3e879bfb7f78a44ae6b67828e1949f7ca2378c4
Ahmad-progr <muhammadahmad.mukhtiar@gmail.com>
2025-07-05 16:02:26 +0500

    Add files via upload

---
+++ Even odd checker.py
@@ -0,0 +1,23 @@
+def main():
+    x = is_valid_int()
+    if x is not "Nill":
+        if is_even(x):
+            print(f'{x} is an even number')
+        else:
+            print(f'{x} is an odd number')
+    
+def is_even(n):
+    return n % 2 == 0
+
+
+#Chatgpt Help Function:
+def is_valid_int():
+    try:
+        n = int(input("Enter a number: "))
+        return n
+    except ValueError:
+        print("Enter a valid integer bitch!")
+        return "Nill"
+
+main()
+
