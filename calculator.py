2c8b1c369dbc182beea1eb911b1b3cf4f55953a4
Ahmad-progr <muhammadahmad.mukhtiar@gmail.com>
2025-07-06 14:07:00 +0500

    Deleted stash

--- calculator.py
+++
@@ -1,9 +0,0 @@
-x = float(input("Enter a number: "))
-y = float(input("Enter a second number: "))
-
-sum = round(x + y)
-print("\n", "sum: ", sum, sep="")  # Prints the sum with commas as thousands separators
-
-
-div = round(x / y, 2)
-print("division: ", div)  # Prints the division result rounded to 3 decimal places
