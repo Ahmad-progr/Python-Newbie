ffc56e91a766ce9667bfbcb067558723eb82d82a
Ahmad-progr <muhammadahmad.mukhtiar@gmail.com>
2025-07-14 22:30:23 +0500

    Created sports indoor or outdoor verification program

---
+++ Week 2/Sports in-out.py
@@ -0,0 +1,22 @@
+games = {
+    "Badminton": "Indoor",
+    "Chess": "Indoor",
+    "Ludo": "Indoor",
+    "Football": "Outdoor",
+    "Basketball": "Outdoor",
+    "Tug o War": "Outdoor",
+    "Volley Ball": "Outdoor",
+    "Poker": "Indoor"
+}
+
+def main():
+    game = input("\nEnter a sports game: ").title()
+    if game in games:
+        print(game_ground(game))
+    else:
+        print("===================================\nGame not found.\n===================================")
+
+def game_ground(game):
+    return f"\n===================================\n\n{game} is an {games[game]} game.\n\n==================================="
+
+main()
